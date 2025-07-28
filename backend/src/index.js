0d6b37ba931f29215df6768ade04370252221e87
zeeshan <084zeeshankhan@gmail.com>
2025-07-28 14:05:12 +0000

    bug fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,37 +1,43 @@
-import express from 'express'
+import express from "express";
 import dotenv from "dotenv";
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import path from 'path';
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
-import { connectDB } from './lib/db.js';
-import authRoutes from './routes/auth.route.js'
-import messageRoutes from './routes/message.route.js'
-import { app } from './lib/socket.js';
+import path from "path";
 
+import { connectDB } from "./lib/db.js";
 
-dotenv.config()
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
 
-app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true
-}))
-app.use(express.json())
-app.use(cookieParser())
-const __dirname=path.resolve()
-app.use('/api/auth', authRoutes)
-app.use('/api/messages', messageRoutes)
+dotenv.config();
 
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
-if(process.env.NODE_ENV==='production'){
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
-  })
+  
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
-const PORT=process.env.PORT
-app.listen(PORT, () => {
-  console.log(`server is running on port ${PORT}`);
-  connectDB()
-})
+
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
+});
--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -1,42 +1,37 @@
-import {createServer} from 'http';
-import express from 'express';
-import {Server} from 'socket.io';
+import { Server } from "socket.io";
+import http from "http";
+import express from "express";
 
-const app=express();
-const httpServer=createServer(app);
+const app = express();
+const server = http.createServer(app);
 
-const io=new Server(httpServer, {
+const io = new Server(server, {
   cors: {
-     origin: 'http://localhost:5173',
-     credentials: true
+    origin: ["http://localhost:5173"],
   },
-})
+});
 
-const onlineUsers = {}
-
-export function getReceiverSocketId(userId){
-  return onlineUsers[userId]
+export function getReceiverSocketId(userId) {
+  return userSocketMap[userId];
 }
 
+// used to store online users
+const userSocketMap = {}; // {userId: socketId}
+
 io.on("connection", (socket) => {
-  console.log("Socket Connected", socket.id)
-  const userId = socket.handshake.query.userId
-  if(userId){
-  onlineUsers[userId]=socket.id;
-  io.emit("getOnlineUsers", Object.keys(onlineUsers))
+  console.log("A user connected", socket.id);
 
+  const userId = socket.handshake.query.userId;
+  if (userId) userSocketMap[userId] = socket.id;
 
-  }
-  
+  // io.emit() is used to send events to all the connected clients
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
   socket.on("disconnect", () => {
-    console.log("Socket User Disconnect", socket.id);
-    delete onlineUsers[userId]
-  })
-})
+    console.log("A user disconnected", socket.id);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  });
+});
 
-
-
-httpServer.listen(process.env.PORT)
-
-export {app, io}
+export { io, app, server };
